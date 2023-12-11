7a7fe344a0535add17501b7a987d4bb3fdbf68f3
Joshua Sanders <joshua.sandersr@gmail.com>
2023-12-11 15:47:43 -0800

    Add demo main file for build testing
    If that demo project with that main can be built and run then
    we haven't broken anything

--- source/package_mgr_demo/main.cpp
+++ source/package_mgr_demo/main.cpp
@@ -1,6 +1,53 @@
+#include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <fstream>
+#include <iostream>
 
-int main()
-{
+// This file is basically just a build test to make sure everything is still hooked up properly.
+
+int main() {
+	// Setup multi-sink logger
+	auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs.txt", true);
+	spdlog::logger logger("multi_sink", { console_sink, file_sink });
+	logger.set_level(spdlog::level::debug); // Set global log level to debug
+	logger.flush_on(spdlog::level::debug);  // Flush to the sinks on each log
+
+	// Log message to console and file
+	logger.info("Starting application");
+
+	// Create and write JSON to file
+	nlohmann::json jsonExample;
+	jsonExample["name"] = "John Doe";
+	jsonExample["age"] = 30;
+	jsonExample["occupation"] = "Software Developer";
+	jsonExample["languages"] = { "C++", "Python", "JavaScript" };
+
+	std::ofstream jsonFile("example.json");
+	if (jsonFile.is_open()) {
+		jsonFile << jsonExample.dump(4);
+		jsonFile.close();
+		logger.info("JSON file written successfully");
+	}
+	else {
+		logger.error("Failed to write JSON file.");
+	}
+
+	// Read JSON from file
+	std::ifstream jsonFileRead("example.json");
+	if (jsonFileRead.is_open()) {
+		nlohmann::json jsonRead;
+		jsonFileRead >> jsonRead;
+		jsonFileRead.close();
+
+		// Log the contents of the JSON object
+		logger.info("JSON file read successfully: {}", jsonRead.dump(4));
+	}
+	else {
+		logger.error("Could not open JSON file for reading.");
+	}
+
 	return 0;
 }
